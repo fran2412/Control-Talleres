02a32c79f6886bdecd13a417aaac4991f30e0398
Francisco <frla241205@gmail.com>
2025-08-27 11:01:49 -0600

    Login

---
+++ Windows/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ControlTalleresMVP.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtPassword.Password == "admin" && txtUsuario.Text == "admin")
+            {
+                MessageBox.Show("¡Inicio de sesión exitoso!", "Éxito", MessageBoxButton.OK, MessageBoxImage.Information);
+                // Open the main application window here
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña inválidos.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
