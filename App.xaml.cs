6ac8928a097747f9bec45efa8488185863c9e0f6
Francisco <frla241205@gmail.com>
2025-08-27 13:25:58 -0600

    Inyección de dependencias

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using ControlTalleresMVP.Windows;
+﻿using ControlTalleresMVP.UI.Windows;
+using Microsoft.Extensions.DependencyInjection;
 using System.Configuration;
 using System.Data;
 using System.Windows;
@@ -10,12 +11,24 @@ namespace ControlTalleresMVP
     /// </summary>
     public partial class App : Application
     {
+        public static IServiceProvider? ServiceProvider { get; private set; }
+
         protected override void OnStartup(StartupEventArgs e)
         {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+
+            ServiceProvider = services.BuildServiceProvider();
+
             base.OnStartup(e);
-            var mainWindow = new MainWindow();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
             mainWindow.Show();
         }
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<MainWindow>();
+            services.AddSingleton<MenuWindow>();
+        }
     }
-
 }
