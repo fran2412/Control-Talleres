e57b7b8ea7b84b213f5d5fc78983b5c446fb7e93
Francisco <frla241205@gmail.com>
2025-09-19 20:14:01 -0600

    Cambio del fondo del logo del menú

--- ViewModel/Menu/MenuInicioViewModel.cs
+++ ViewModel/Menu/MenuInicioViewModel.cs
@@ -1,13 +1,28 @@
-﻿using System;
+﻿using ControlTalleresMVP.Abstractions;
+using ControlTalleresMVP.Helpers.Commands;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ControlTalleresMVP.ViewModel.Menu
 {
     public class MenuInicioViewModel
     {
+        public INavigatorService Navigator { get; }
+        public ICommand GoAlumnos { get; }
+        public ICommand GoTalleres { get; }
+        public ICommand GoPagos { get; }
 
+        public MenuInicioViewModel(INavigatorService navigator)
+        {
+            Navigator = navigator;
+            
+            GoAlumnos = new RelayCommand(() => Navigator.NavigateTo<MenuAlumnosViewModel>());
+            GoTalleres = new RelayCommand(() => Navigator.NavigateTo<MenuTalleresViewModel>());
+            GoPagos = new RelayCommand(() => Navigator.NavigateTo<MenuPagosViewModel>());
+        }
     }
 }
