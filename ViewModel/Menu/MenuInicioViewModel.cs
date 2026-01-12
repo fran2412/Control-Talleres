a18f00aab984dea17a8dee23f6890107280ce03b
Francisco <panchitolop100@gmail.com>
2026-01-12 09:05:28 -0600

    Feat: Texto de bienvenida al sistema ahora muestra la sede seleccionada

--- ViewModel/Menu/MenuInicioViewModel.cs
+++ ViewModel/Menu/MenuInicioViewModel.cs
@@ -1,19 +1,23 @@
 ﻿using ControlTalleresMVP.Abstractions;
 using ControlTalleresMVP.Helpers.Commands;
+using ControlTalleresMVP.Services.Sesion;
 using System.Windows.Input;
 
 namespace ControlTalleresMVP.ViewModel.Menu
 {
     public class MenuInicioViewModel
     {
+        private readonly ISesionService _sesionService;
+        public string TextoBienvenida => $"¡Bienvenido al Sistema!\nSede {_sesionService.ObtenerNombreSede()}";
         public INavigatorService Navigator { get; }
         public ICommand GoAlumnos { get; }
         public ICommand GoTalleres { get; }
         public ICommand GoPagos { get; }
 
-        public MenuInicioViewModel(INavigatorService navigator)
+        public MenuInicioViewModel(INavigatorService navigator, ISesionService sesionService)
         {
             Navigator = navigator;
+            _sesionService = sesionService;
 
             GoAlumnos = new RelayCommand(() => Navigator.NavigateTo<MenuAlumnosViewModel>());
             GoTalleres = new RelayCommand(() => Navigator.NavigateTo<MenuTalleresViewModel>());
